8e3000c3eca19b9f05d7bd3916a485a3cf468d45
sachingupta7557 <sachintend66@gmail.com>
2025-04-14 09:11:46 +0530

    navbar

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,36 +1,4 @@
-// import React, { useContext } from 'react'
-// import { assets } from '../assets/assets'
-// import { AdminContext } from '../context/AdminContext'
-// import {useNavigate} from 'react-router-dom'
-// import { TutorContext } from '../context/TutorContext'
 
-// const Navbar = () => {
-
-//     const {aToken,setAToken} = useContext(AdminContext)
-//     const {dToken,setDToken} = useContext(TutorContext)
-
-//     const navigate = useNavigate()
-
-//     const logout = () => {
-//         navigate('/')
-//         aToken && setAToken('')
-//         aToken && localStorage.removeItem('aToken')
-//         dToken && setDToken('')
-//         dToken && localStorage.removeItem('dToken')
-
-//     }
-//   return (
-//     <div className='flex justify-between items-center px-4 sm:px-10 py-3 border-b bg-white'>
-//         <div className='flex items-start gap-2 text-xs'>
-//             <img className='w-16 sm:w-22 cursor-pointer' src={assets.admin_logoo} alt="" />
-//             <p className='border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-600'> {aToken ? 'Admin' : 'Tutor'}</p>
-//         </div>
-//         <button onClick={logout} className='bg-primary text-white text-sm px-10 py-2 rounded-full'>Logout</button>
-//     </div>
-//   )
-// }
-
-// export default Navbar
 
 import React, { useContext } from 'react';
 import { assets } from '../assets/assets';
