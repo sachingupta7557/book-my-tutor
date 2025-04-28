fb0e4a7d7aca7f02a194db7793bcf866fde71361
sachingupta7557 <sachintend66@gmail.com>
2025-04-28 06:51:11 +0530

    changes

--- admin/src/context/AppContext.jsx
+++ admin/src/context/AppContext.jsx
@@ -1,42 +1,96 @@
+// import { createContext } from "react";
+
+// export const AppContext = createContext()
+
+// const AppContextProvider = (props) => {
+
+//   const currency = '$'
+
+//   const calculateAge = (dob) => {
+
+//     const today = new Date()
+//     const birthDate = new Date(dob)
+
+//     let age = today.getFullYear() - birthDate.getFullYear()
+//     return age
+
+//   }
+
+//   const months = ["", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+
+
+//   const slotDateFormat = (slotDate) => {
+//     const dateArray = slotDate.split('_')
+//     return `${dateArray[0]} ${months[Number(dateArray[1])]} ${dateArray[2]}`
+//   }
+
+
+//   const value = {
+//     calculateAge,
+//     slotDateFormat,
+//     currency
+//   }
+
+//   return (
+//     <AppContext.Provider value={value}>
+//       {props.children}
+//     </AppContext.Provider>
+//   )
+
+// }
+// export default AppContextProvider
+
 import { createContext } from "react";
 
-export const AppContext = createContext()
+export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
 
-  const currency = '$'
+  const currency = '$';
 
   const calculateAge = (dob) => {
+    if (!dob) return null; // No DOB provided
+    const birthDate = new Date(dob);
+    if (isNaN(birthDate.getTime())) return null; // Invalid DOB
 
-    const today = new Date()
-    const birthDate = new Date(dob)
+    const today = new Date();
+    let age = today.getFullYear() - birthDate.getFullYear();
 
-    let age = today.getFullYear() - birthDate.getFullYear()
-    return age
+    // Adjust if birthday hasn't occurred yet this year
+    const monthDifference = today.getMonth() - birthDate.getMonth();
+    if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+    }
 
-  }
+    return age;
+  };
 
-  const months = ["", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-
-
+  const months = ["", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
   const slotDateFormat = (slotDate) => {
-    const dateArray = slotDate.split('_')
-    return `${dateArray[0]} ${months[Number(dateArray[1])]} ${dateArray[2]}`
-  }
+    if (!slotDate) return "Invalid Date";
 
+    const dateArray = slotDate.split('_');
+    if (dateArray.length !== 3) return "Invalid Date";
+
+    const monthIndex = Number(dateArray[1]);
+    if (monthIndex < 1 || monthIndex > 12) return "Invalid Date";
+
+    return `${dateArray[0]} ${months[monthIndex]} ${dateArray[2]}`;
+  };
 
   const value = {
     calculateAge,
     slotDateFormat,
     currency
-  }
+  };
 
   return (
     <AppContext.Provider value={value}>
       {props.children}
     </AppContext.Provider>
-  )
+  );
+};
 
-}
-export default AppContextProvider
+export default AppContextProvider;
