cace2fe4d085b28b9803dd5b12216d1e1dbf6b74
sachingupta7557 <144935207+sachingupta7557@users.noreply.github.com>
2025-11-05 15:08:47 +0530

    Update AdminContext.jsx

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -12,7 +12,7 @@ const AdminContextProvider = (props) => {
   const [dashData, setDashData] = useState(false);
 
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
-  // const backendUrl = "https://book-my-tutor-2vfu.onrender.com"
+  
 
   
   const authHeaders = {
