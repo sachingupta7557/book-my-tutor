584ac4b3fec1f428a2591bea2c88b77a9ff23878
sachingupta7557 <144935207+sachingupta7557@users.noreply.github.com>
2025-11-05 15:10:14 +0530

    Update AppContext.jsx

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -8,7 +8,7 @@ const AppContextProvider = (props) => {
 
     const currencySymbol = '$'
     const backendUrl = import.meta.env.VITE_BACKEND_URL
-   // const backendUrl = "https://book-my-tutor-2vfu.onrender.com"
+   
 
     const [tutors,setTutors] = useState([])
     const [token,setToken] = useState(localStorage.getItem('token')?localStorage.getItem('token'):false)
